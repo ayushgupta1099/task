84d0bba98f28d80e8fc59e12e684328c502ccf24
ayushgupta1099 <ayushgupta1099@gmail.com>
2020-04-21 01:10:18 +0530

    condition added

--- task-a.js
+++ task-a.js
@@ -7,3 +7,16 @@ const print = () => {
   console.log(fullname);
 };
 print();
+
+let day = "sunday";
+if (day === "sunday" || day === "saturday") {
+  console.log("Weekend");
+} else console.log("Weekday");
+
+let num = 1;
+let i;
+if (num === 5) {
+  for (i = 1; i <= 100; i++) {
+    if (i % 5 === 0) console.log(i);
+  }
+} else console.log("Error");
