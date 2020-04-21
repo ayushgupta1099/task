c0ea164c3ac9ad2018386872c72f1997c57ca1e1
ayushgupta1099 <ayushgupta1099@gmail.com>
2020-04-21 11:50:16 +0530

    corrections

--- task-a.js
+++ task-a.js
@@ -1,6 +1,7 @@
 let fname = "Ayush";
 let lname = "Gupta";
 let fullname = fname.concat(lname);
+fullname = fullname.toUpperCase();
 let len = fullname.length;
 
 const print = () => {
@@ -8,15 +9,19 @@ const print = () => {
 };
 print();
 
-let day = "sunday";
-if (day === "sunday" || day === "saturday") {
-  console.log("Weekend");
-} else console.log("Weekday");
+let x = 1;
+switch (x) {
+  case 1:
+    let day = "sunday";
+    if (day === "sunday" || day === "saturday") {
+      console.log("Weekend");
+    } else console.log("Weekday");
+    break;
 
-let num = 1;
-let i;
-if (num === 5) {
-  for (i = 1; i <= 100; i++) {
-    if (i % 5 === 0) console.log(i);
-  }
-} else console.log("Error");
+  case 2:
+    for (let i = 1; i <= 100; i++) if (i % 5 === 0) console.log(i);
+    break;
+
+  default:
+    console.log("Wrong Choice");
+}
